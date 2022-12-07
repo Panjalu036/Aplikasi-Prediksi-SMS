315e0e4385b8f262b1f0cc8049c1858ab90777dd
Alif Fajar Panjalu <100903176+Panjalu036@users.noreply.github.com>
2022-12-07 22:02:41 +0700

    .py
    Aplikasi Update

--- stream-nlp.py
+++ stream-nlp.py
@@ -3,11 +3,11 @@ import streamlit as st
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 # load save model
-model_fraud = pickle.load(open('model_fraud.sav','rb'))
+model_fraud = pickle.load(open('model_Predikksi_SMS.sav','rb'))
 
 tfidf = TfidfVectorizer
 
-loaded_vec = TfidfVectorizer(decode_error="replace", vocabulary=set(pickle.load(open("new_selected_feature_tf-idf.sav", "rb"))))
+loaded_vec = TfidfVectorizer(decode_error="replace", vocabulary=set(pickle.load(open("fitur_baru-_tf-idf.sav", "rb"))))
 
 
 # judul halaman
